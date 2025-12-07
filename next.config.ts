6675ebb1a8223472378add593ecc975ad483b7f3
joonhoekim <26rote@gmail.com>
2025-12-07 23:15:29 +0900

    update: nextjs 15 to 16 and other dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   // next-mdx-remote를 사용하므로 기본 Next.js 설정만 유지
   experimental: {
